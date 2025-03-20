5ea79b8906320f38636cb61f62cf6f1922e528cc
Kalique <kalique@gmail.com>
2025-03-20 17:31:58 +0200

    и ещё одна правка для gh pages

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import UserDetailsPage from "./pages/UserDetailsPage"
 
 function App() {
     return (
-        <Router>
+        <Router basename="/kode-intership-2025-react">
             <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/:id" element={<UserDetailsPage />} />
